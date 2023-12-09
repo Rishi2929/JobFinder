fdcae64e140b322402781fad3f0c51542cbac62d
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-12-09 14:56:30 +0530

    added database config and initialzation

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from "express"
 import { config } from 'dotenv';
-
-
+import { connectDB } from "./data/database.js";
 
 export const app = express();
 config({
     path: "./data/config.env"
 })
+connectDB()
 //Middleware
 app.use(express.json())
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'Active',
+        message: 'Server is healthy and running.',
+    });
+});
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
